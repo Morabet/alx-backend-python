9366e9ff5338e578cde1d60cbfe17955ca13bb8c
Morabet <ismailelmorabet2@gmail.com>
2024-05-13 13:02:00 +0000

    fix com

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" A module for testing the utils module"""
+"""A module for testing the utils module"""
 
 import unittest
 from parameterized import parameterized, parameterized_class
